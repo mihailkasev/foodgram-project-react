9bead2e54d879392d1de6255417b1c76be5a167f
mihailkasev <kasevmihail9@gmail.com>
2022-12-27 05:00:50 +0300

    Исправления flake8 и isort

--- backend/api/views.py
+++ backend/api/views.py
@@ -11,16 +11,15 @@ from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from recipes.models import (Cart, Favorite, Ingredient, IngredientInRecipe,
-                            Recipe, Tag)
-from users.models import Subscription, User
-
 from .filters import IngredientSearchFilter, RecipeFilterSet
 from .permissions import AdminOrReadOnly, RecipePermission
 from .serializers import (CartSerializer, FavoriteSerializer,
                           IngredientSerializer, RecipeReadSerializer,
                           RecipeWriteSerializer, SubscriptionListSerializer,
                           SubscriptionSerializer, TagSerializer)
+from recipes.models import (Cart, Favorite, Ingredient, IngredientInRecipe,
+                            Recipe, Tag)
+from users.models import Subscription, User
 
 
 class IngredientViewSet(ModelViewSet):
