dc5af0b47340185ce1284060cbd17abe3114c881
mihailkasev <kasevmihail9@gmail.com>
2022-12-27 04:50:44 +0300

    Исправления flake8 и isort

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,6 +1,6 @@
 from colorfield.fields import ColorField
-from django.db import models
 from django.core.validators import MinValueValidator
+from django.db import models
 
 from users.models import User
 
